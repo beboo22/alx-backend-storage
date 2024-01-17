979ad096828249d62e1568bb39ebac2ef6ab6a83
beboo22 <mt557829@gmail.com>
2024-01-17 19:31:51 +0200

    task

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE TABLE IF NOT EXISTS users (
+    id INT,
+    email VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255)
+);
