d7654c6ac9b95896d0cb1385337e1537d30d3489
beboo22 <mt557829@gmail.com>
2024-01-17 21:29:27 +0200

    task

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER TEG1
+BEFORE UPDATE ON users
+FOR EACH ROW 
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+DELIMITER ;
