4558baaafd77006bdb9d3cfb13c9ab0ca9d99a78
beboo22 <mt557829@gmail.com>
2024-01-17 20:20:07 +0200

    task

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
 -- 3 first students in the Batch ID=3
-SELECT origin AS origin , fans AS nb_fans
+SELECT origin AS origin , sum(fans) AS nb_fans
 From metal_bands
+GROUP BY origin
 ORDER BY nb_fans DESC;
